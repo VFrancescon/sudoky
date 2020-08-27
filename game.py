360ae02b04d0c2062b2728e06c718a025e73701b
VFrancescon <67584147+VFrancescon@users.noreply.github.com>
2020-08-27 18:50:04 +0100

    Update game.py

--- game.py
+++ game.py
@@ -21,8 +21,9 @@ y_counter = 0
 
 surface.fill((255,255,255))
 
+grid.pre_print_grid(surface)
+
 while running:
-    #grid.draw(surface)
     grid.draw(surface)
     pygame.display.flip()
 
@@ -31,63 +32,77 @@ while running:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
+            lclick = pos[0] // 60
+            rclick = pos[1] // 60
             if pygame.mouse.get_pressed()[0] or pygame.mouse.get_pressed()[2]:
                 if pygame.mouse.get_pressed()[0] == True: l_click = True
                 if pygame.mouse.get_pressed()[2] == True: r_click = True
-                if grid.get_cell_value(pos[0] // 60, pos[1] // 60) == 0:
-                    grid.red_box(pos[0] // 60, pos[1] // 60, surface)
+                if grid.get_cell_value(lclick, rclick) == 0:
+                    grid.red_box(lclick, rclick, surface)
             if pygame.mouse.get_pressed()[1]:
-                grid.clear_box(pos[0] // 60, pos[1] // 60, surface)
+                grid.clear_box(lclick, rclick, surface)
 
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_KP1 or event.key == pygame.K_1:
                 inputk = 1
-            if event.key == pygame.K_KP2 or event.key == pygame.K_2:
+            elif event.key == pygame.K_KP2 or event.key == pygame.K_2:
                 inputk = 2
-            if event.key == pygame.K_KP3 or event.key == pygame.K_3:
+            elif event.key == pygame.K_KP3 or event.key == pygame.K_3:
                 inputk = 3
-            if event.key == pygame.K_KP4 or event.key == pygame.K_4:
+            elif event.key == pygame.K_KP4 or event.key == pygame.K_4:
                 inputk = 4
-            if event.key == pygame.K_KP5 or event.key == pygame.K_5:
+            elif event.key == pygame.K_KP5 or event.key == pygame.K_5:
                 inputk = 5
-            if event.key == pygame.K_KP6 or event.key == pygame.K_6:
+            elif event.key == pygame.K_KP6 or event.key == pygame.K_6:
                 inputk = 6
-            if event.key == pygame.K_KP7 or event.key == pygame.K_7:
+            elif event.key == pygame.K_KP7 or event.key == pygame.K_7:
                 inputk = 7
-            if event.key == pygame.K_KP8 or event.key == pygame.K_8:
+            elif event.key == pygame.K_KP8 or event.key == pygame.K_8:
                 inputk = 8
-            if event.key == pygame.K_KP9 or event.key == pygame.K_9:
+            elif event.key == pygame.K_KP9 or event.key == pygame.K_9:
                 inputk = 9
-            if event.key == pygame.K_r:
+            elif event.key == pygame.K_r:
                 inputk = 'r'
-            if event.key == pygame.K_c:
+            elif event.key == pygame.K_c:
                 inputk = 'c'
-            if event.key == pygame.K_b:
+            elif event.key == pygame.K_b:
                 inputk = 'b'
+            elif event.key == pygame.K_n:
+                inputk = 'n'
+            elif event.key == pygame.K_g:
+                inputk = 'g'
+            elif event.key == pygame.K_s:
+                inputk = 's'
 
             if isinstance(inputk, str):
                 if inputk == 'r':
-                    grid.get_row(pos[1] // 60)
-                if inputk == 'c':
-                    grid.get_col(pos[0] // 60)
-                if inputk == 'b':
-                    grid.get_box(pos[0] // 60, pos[1] // 60)                    
+                    grid.dc_checker(lclick, rclick)
+                elif inputk == 'c':
+                    grid.clear_board(surface)
+                elif inputk == 'b':
+                    grid.get_comments(lclick, rclick)
+                elif inputk == 'n':
+                    grid.naked_single(lclick, rclick, surface)
+                    grid.naked_double(lclick,rclick,surface)
+                elif inputk == 'g':
+                    grid.solver(surface)
+                elif inputk == 's':
+                    grid.generate_candidates(surface)                     
 
             if isinstance(inputk, int):
                 if l_click == True:
-                    if grid.get_cell_value(pos[0] // 60, pos[1] // 60) == 0:
-                        grid.set_cell_value(pos[0] // 60, pos[1] // 60, inputk)
-                        grid.print_value(surface, pos[0] // 60, pos[1] // 60)
+                    if grid.get_cell_value(lclick, rclick) == 0:
+                        #grid.set_cell_value(lclick, rclick, inputk)
+                        grid.print_value(surface, lclick, rclick, inputk)
                     l_click = False
                     r_click = False
 
-                if r_click == True:
-                    grid.print_comment(surface, pos[0] // 60, pos[1] // 60, inputk)
+                elif r_click == True:
+                    grid.print_comment(surface, lclick, rclick, inputk)
                     l_click = False
                     r_click = False
         
 
 
-            #grid.print_grid()
 
